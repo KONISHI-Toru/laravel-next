de7c4d3ed9b3e0f9226c9b0e7120c2092f11cee8
KONISHI Toru <sting@nifty.com>
2022-03-10 15:46:16 +0900

    サンプルのAPIを呼び出すサンプル画面実装。

---
+++ src-next/libs/axios.ts
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+const axios = Axios.create({
+    baseURL: process.env.API_URL,
+    headers: {
+	"X-Requested-With": "XMLHttpRequest",
+    },
+    withCredentials: true,
+});
+
+
+export default axios;
